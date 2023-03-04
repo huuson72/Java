8587a0dd5e589dab2687134827aaad6db5088431
huuson.series <huuson0702@gmail.com>
2023-03-04 21:09:31 +0700

    upload

--- src/OOP/Main.java
+++ src/OOP/Main.java
@@ -1,5 +1,10 @@
 package OOP;
 public class Main {
-  Employee e = new Employee(01, "Nguyen", "Nam", 1000);
+  public static void main(String[] args) {
+    Employee e = new Employee(01, "Nguyen", "Nam", 1000);
+    System.out.println(e.toString());
+    
+  }
+  
   
 }
